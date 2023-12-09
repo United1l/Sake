be3010200121233b6c1d9484c586a452389e506f
Michael Asare <55958142+United1l@users.noreply.github.com>
2023-12-09 20:35:36 +0000

    refined the css the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import Button from './components/buttons'
 import { imgSrc } from './landing_image'
 
 export default function Home() {
+
   return (
     <div className="min-h-screen flex flex-col bg-[#adb5bd] text-white">
       <header className="w-full h-16 flex items-center justify-between px-6">
         <div className="flex flex-1 space-x-2 items-center">
           <div className="w-6 h-6 bg-red-400 rounded-full"></div>
-          <h1 className="text-xl font-medium">Sake</h1>
+          <h1 className="text-xl md:text-[2rem] font-medium">Sake</h1>
         </div>
-        <div className="flex flex-1 justify-evenly text-xs font-medium">
+        <div className="flex flex-1 justify-evenly text-xs md:text-xl font-medium">
           <h5 className="cursor-pointer">Bar</h5>
           <h5 className="cursor-pointer">Collection</h5>
         </div>
@@ -21,19 +22,20 @@ export default function Home() {
         </div>
       </header>
       <main className="flex flex-1 flex-col md:flex-row">
-      <div className="w-full md:w-2/5 h-3/5 md:h-auto flex items-center justify-center">
+      <div className="w-full md:w-2/5 h-[30rem] md:h-auto flex items-center justify-center">
           <Image
             src={imgSrc} 
             alt="A glass of alcohol"
             height={400}
             width={500}
+            className="h-auto w-auto md:h-[30rem] md:w-[32rem]"
           />
         </div>
-        <div className="flex-1 flex flex-col justify-center p-2">
-          <h2 className="text-xl font-semibold text-center">
+        <div className="flex-1 flex flex-col justify-start md:justify-center p-2">
+          <h2 className="text-xl font-semibold text-center sm:text-[1.5rem] md:text-[3rem] leading-snug">
             The fastest new way to get Alcoholic drinks to your place
           </h2>
-          <h5 className="text-xs text-center mt-3">
+          <h5 className="text-xs text-center mt-3 md:text-[1.3rem] leading-snug">
             We're the current best way to get booze online and safely delivered
             to your location
             </h5>
